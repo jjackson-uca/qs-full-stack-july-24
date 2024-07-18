a8168f26237c50cf4c9e3a8980125e9e456287c3
Samuel Pruitt <pruitt.sam@gmail.com>
2024-07-18 18:20:28 -0500

    adding first webpage

---
+++ sam/test_web/samjs.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('div.cs img').hoverpulse({
+        size: 40,  // number of pixels to pulse element (in each direction)
+        speed: 400 // speed of the animation 
+    });
+});
