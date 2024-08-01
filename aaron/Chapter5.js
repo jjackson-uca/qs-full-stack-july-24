d53df8e975d58569753997968f3bec7b84b3e293
Aaron Townsend <aartowns7@gmail.com>
2024-08-01 18:09:37 -0500

    Chapter 5

---
+++ aaron/Chapter5.js
@@ -0,0 +1,52 @@
+//exercise1
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(arrays.reduce((flat, current) => flat.concat(current), []));
+
+//exercise2
+
+function loop(start, test, update, body) {
+  for (let value = start; test(value); value = update(value)) {
+    body(value);
+  }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+
+//exercise3
+
+function every(array, predicate) {
+  for (let element of array) {
+    if (!predicate(element)) return false;
+  }
+  return true;
+}
+
+function every2(array, predicate) {
+  return !array.some(element => !predicate(element));
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+
+console.log(every([2, 4, 16], n => n < 10));
+
+console.log(every([], n => n < 10));
+
+//exercise4
+
+function dominantDirection(text) {
+  let counted = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({name}) => name != "none");
+
+  if (counted.length == 0) return "ltr";
+
+  return counted.reduce((a, b) => a.count > b.count ? a : b).name;
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
